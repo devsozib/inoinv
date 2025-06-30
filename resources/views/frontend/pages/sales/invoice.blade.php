9d36684b24886e8e085797d266862744057049f3
MD Shawon Miah <shawonmahmodul12@gmail.com>
2025-06-30 08:06:13 -0700

    dashboard

--- resources/views/frontend/pages/sales/invoice.blade.php
+++ resources/views/frontend/pages/sales/invoice.blade.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Quick Phone Fix N More</title>
+    <title>Inoodex Inventory</title>
     <style>
       @page {
         size: auto;
